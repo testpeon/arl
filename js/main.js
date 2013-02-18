54b210d483feed082c9a114a472aaecbebc6f0c4
testpeon <testpeon.do@gmail.com>
2013-02-18 17:04:49 +0000

    Added backup from workstation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+var WebFontConfig;
+WebFontConfig = { google: { families: ['Open+Sans+Condensed:700:latin'] } };
+
+(function() {
+	"use strict";
+	var wf = document.createElement('script');
+	wf.src = ('https:' === document.location.protocol ? 'https' : 'http') +
+	'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
+
 $(document).ready(function() {
   "use strict";
-  window.alert('go');
+ 
 });
